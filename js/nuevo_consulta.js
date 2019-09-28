202e7affd575e4dcd0708c8128d47e6b00db4cc6
alejandro <alejhandro0320@gmail.com>
2019-09-27 20:41:08 -0500

    Version2

--- js/nuevo_consulta.js
+++ js/nuevo_consulta.js
@@ -8,7 +8,26 @@ fetch("https://swapi.co/api/people/")
 
 .then(json => {
     datas = json
-   
-    console.log(datas.results)
+    
+    let list = ""
+    let tabla = ""
+    for (var i in datas.results) {
+        console.log(datas.results[i].name)
+       list += `<option> ${datas.results[i].name} </option>`
+       tabla += `<tr>`
+       tabla += `<td> ${datas.results[i].name}</td>`  
+       tabla += `<td>${datas.results[i].hair_color}</td>` 
+       tabla += `<td>${datas.results[i].birth_year}</td>`
+       tabla += `</tr>`          
+    }
+    document.getElementById("listado").innerHTML=list;
+    document.getElementById("tabla").innerHTML=tabla;
+    //userList.querySelectorAll(".tabla").innerHTML=tabla;
+    document.getElementById("listados").innerHTML=tabla;
 
+    var i=0
+   while (i<datas.results.length) {
+    console.log(datas.results[i].hair_color)
+       i++
+   }
 })
